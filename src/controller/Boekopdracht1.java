e9e83f360dcdaedff949facd86a6ca57bc49b0ac
Pitycake <berndtwoest@hotmail.com>
2024-10-09 07:57:13 -0400

    rekensommetje

---
+++ src/controller/Boekopdracht1.java
@@ -0,0 +1,13 @@
+package controller;
+
+public class boekopdracht1 {
+    public static void main(string[] args) {
+        double i = 50.0;
+        double k = i + 50.0;
+        double j = k + 1;
+
+        System.out.println("j is " + j + " and " +
+        "k is " + k);
+
+    }
+}
